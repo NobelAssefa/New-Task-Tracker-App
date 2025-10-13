a4f47a90c8933e9cce228dc5f5d5dd461721c14d
Abel <abelnati124@gmail.com>
2025-10-13 16:37:56 +0400

    Stundet A setup (filter and sort view),  (add task view), (theme button)

--- New-Task-Tracker-App/Controller/ListOfTasksViewController.swift
+++ New-Task-Tracker-App/Controller/ListOfTasksViewController.swift
@@ -11,11 +11,27 @@ class ListOfTasksViewController: UIViewController {
     
     let halfView = UIView()
     let lable = UILabel()
+    var stackView = UIStackView()
+    let button1 = UIButton(type: .system)
+    let textField1 = UITextField()
+    
+    var filterStackView = UIStackView()
+    let filterButton = UIButton(type: .system)
+    let sortButton = UIButton(type: .system)
+    
+    
+    let themeButton = UIButton(type: .system)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpHalfViewController()
         setupLable()
+        setupthemeButton()
+        setupFilterButton()
+        setUpStackView()
+        
+        setUpStackView()
     }
 
     func setUpHalfViewController(){
@@ -48,6 +64,85 @@ class ListOfTasksViewController: UIViewController {
             lable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             lable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
         ])
+        
+    }
+    
+    func setupFilterButton(){
+        sortButton.setImage(Images.arrow_up_arrow_down, for: .normal)
+        filterButton.setImage(Images.sortingButtonIcon, for: .normal)
+//        sortButton.addTarget(self, action: #selector(sortButtonTapped), for: .touchUpInside)
+//        filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+        filterButton.translatesAutoresizingMaskIntoConstraints = false
+        sortButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        filterButton.tintColor = Colors.primaryColor
+        sortButton.tintColor = Colors.primaryColor
+        let uiView = UIView()
+        filterStackView.axis = .horizontal
+        filterStackView = UIStackView(arrangedSubviews: [uiView,filterButton,sortButton])
+        
+        uiView.translatesAutoresizingMaskIntoConstraints = false
+        filterStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filterStackView)
+        NSLayoutConstraint.activate([
+            
+            sortButton.heightAnchor.constraint(equalToConstant: 35),
+            sortButton.widthAnchor.constraint(equalToConstant: 80),
+            filterButton.heightAnchor.constraint(equalToConstant: 35),
+            filterButton.widthAnchor.constraint(equalToConstant: 80),
+            
+            filterStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            filterStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            filterStackView.topAnchor.constraint(equalTo: halfView.bottomAnchor,constant: 10)
+        ])
+        
+        
+    }
+    func setupthemeButton(){
+        
+        themeButton.setImage(Images.sun_max, for: .normal)
+        themeButton.tintColor = .white
+        themeButton.translatesAutoresizingMaskIntoConstraints = false
+//        themeButton.addTarget(self, action: #selector(setLighttheme), for: .touchUpInside)
+        view.addSubview(themeButton)
+        
+        NSLayoutConstraint.activate([
+            themeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            themeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        ])
+        
+    }
+    
+    func setUpStackView(){ //50*35
+        
+        
+        button1.setImage(Images.addTaskIcon, for: .normal)
+//        button1.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+//        
+        button1.layer.cornerRadius = 10
+        button1.tintColor = Colors.primaryColor
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        textField1.translatesAutoresizingMaskIntoConstraints = false
+        textField1.placeholder = " Add Task"
+        textField1.backgroundColor = .systemGray5
+        textField1.layer.cornerRadius = 10
+        
+        stackView.axis = .horizontal
+        stackView = UIStackView(arrangedSubviews: [textField1,button1])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            button1.heightAnchor.constraint(equalToConstant: 35),
+            button1.widthAnchor.constraint(equalToConstant: 80),
+            textField1.heightAnchor.constraint(equalToConstant: 100),
+            stackView.heightAnchor.constraint(equalToConstant: 40),
+            stackView.topAnchor.constraint(equalTo: filterStackView.bottomAnchor,constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: 10)
+            
+        ])
+        
         
     }
 }
