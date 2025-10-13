b6f30cb44ffd21ab9d5cab3be05d3533190db955
Abel <abelnati124@gmail.com>
2025-10-13 15:26:36 +0400

    Initial Commit

---
+++ New-Task-Tracker-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New-Task-Tracker-App
+//
+//  Created by Abel Assefa on 13/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
